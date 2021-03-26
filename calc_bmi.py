a4f08cb30a844e4671d53df728c5c10f7e707c52
phat nguyen <batphat98@gmail.com>
2021-03-26 11:20:00 +0900

    bmi計算

---
+++ calc_bmi.py
@@ -0,0 +1,4 @@
+def bmi(height,weight):
+    ok = weight/(height*height)
+    return ok
+
---
+++ main.py
@@ -0,0 +1,10 @@
+import calc_bmi
+h = float(input("身長："))
+w = float(input("体重："))
+print(calc_bmi.bmi(h,w))
+if calc_bmi.bmi(h,w)<18.5:
+    print("食生活")
+elif calc_bmi.bmi(h,w)>30:
+    print("運動不足")
+
+
