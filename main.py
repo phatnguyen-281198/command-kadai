7f5d17df7d62869015e9f08834090104847de289
phat nguyen <batphat98@gmail.com>
2021-03-26 11:27:36 +0900

    間違ったメッセージを修正しました

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import calc_bmi
 h = float(input("身長："))
 w = float(input("体重："))
 print(calc_bmi.bmi(h,w))
+print(calc_bmi.bmi(h,w))
 if calc_bmi.bmi(h,w)<18.5:
     print("食生活")
 elif calc_bmi.bmi(h,w)>30:
